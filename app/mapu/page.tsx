1439a58f9259bbae82f077a831e5cd8a68f9205a
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2024-07-13 01:46:01 -0400

    add: adding input elements for marker

--- app/mapu/page.tsx
+++ app/mapu/page.tsx
@@ -20,12 +20,12 @@ export default async function MapNotSignedIn() {
     .order("inserted_at", { ascending: false });*/
 
   return (
-    <section className="p-3 pt-6 max-w-4xl w-full flex flex-col gap-4">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
+    <section className="p-3 pt-6 max-w-4xl w-full flex flex-col gap-16">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center mt-10">
         Interactive World Map
       </h1>
       <Separator className="w-full" />
-      <div className="h-[70vh] max-h-full border-black rounded-lg margin w-full" style={{borderRadius:'8px', overflow:'hidden'}}>
+      <div className="h-[70vh] mt-20 max-h-full border-black rounded-lg margin w-full" style={{borderRadius:'8px', overflow:'hidden'}}>
         <Worldmap authorized={user !== null}/>
       </div>
     </section>
--- components/world-map.tsx
+++ components/world-map.tsx
@@ -3,18 +3,36 @@ import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaf
 import "leaflet/dist/leaflet.css";
 import { Icon } from "leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster"; 
-import { useState } from 'react';
+import { ImgHTMLAttributes, useState } from 'react';
 import { Dialog } from 'primereact/dialog';
 import { createClient } from "@/utils/supabase/server";
-
+import ResearchType from "@/public/ResearchTypes.json";
+import { InputText } from 'primereact/inputtext';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { Dropdown } from 'primereact/dropdown';
+import {FloatLabel} from 'primereact/floatlabel';
+import "primereact/resources/themes/md-light-deeppurple/theme.css";
 type worldMapProps = {
     authorized: boolean | null;
 }
+type researchType = {
+    "name":string
+    "code":string
+}
+
 export default function Worldmap( {authorized} : worldMapProps) {
-    const [initialPosition, setInitialPosition] = useState<[number, number]>([31.3909,74.2417]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
     const [showInputDiv, setShowInputDiv] = useState(false);
     const [visible, setVisible] = useState(false);
+
+    //user input
+    const [fullName, setFullName] = useState<string|null>(null);
+    const [title,setTitle] = useState<string|null>(null);
+    const [research,setResearch] = useState<researchType|null>(null);
+    const [img,uploadImg] = useState<ImageBitmap|null>(null);
+    const [locationCoordinates,setLocationCoordinates] = useState<[number,number]|null>(null);
+    const [linkedinLink,setLinkedInLink] = useState<string|null>(null);
+    //user input end
 
 
     
@@ -22,10 +40,19 @@ export default function Worldmap( {authorized} : worldMapProps) {
         setVisible(true);
         return showInputDiv ? (
             <>
-                <Dialog className="bg-white w-[50vw]" header="Input your details!" visible={visible} position="top"  onHide={() => {if (!visible) return; setVisible(false); }}>
-                    <p className="m-0">
-                        Example demo for input.
-                    </p>
+                <Dialog className="w-[50vw] flex flex-grow justify-center gap-6" header="Input your details" visible={visible} position="top"  onHide={() => {if (!visible) return; setVisible(false); }}>
+                    <p>Please make sure the marker on the map is accurate to the location you would like to set! :)</p>
+                    <FloatLabel>
+                        <InputText className="h-30 w-30 shadow shadow-slate-500" id="name" value={fullName} onChange={(e)=>setFullName(e.target.value)}/>
+                        <label htmlFor="name">Enter your full name!</label>
+                    </FloatLabel>
+                    <InputText className="h-30 w-30 " id="name" value={fullName} onChange={(e)=>setFullName(e.target.value)}/>
+                    <Dropdown
+                        value={research}
+                        options={ResearchType.map((type) => ({ label: type.name, value: type.code }))}
+                        onChange={(e) => setResearch(e.value)}
+                        placeholder="Select a research"
+                    />
                 </Dialog>
             </>
         ) : null;
@@ -70,7 +97,7 @@ export default function Worldmap( {authorized} : worldMapProps) {
                 <Markers />
                 <MarkerClusterGroup chunkedLoading></MarkerClusterGroup>
             </MapContainer>
-            <AddUserMarker /> {/* Render the input div */}
+            <AddUserMarker/> {/* Render the input div */}
         </>
     );
 }
